d00d374d9bc3ab0d90071dac07d326e45df41010
Pavel Laukhin <pawel.lauhin@yandex.ru>
2023-01-22 22:58:49 +0300

    class DB

---
+++ Classes/DB.php
@@ -0,0 +1,29 @@
+<?php
+
+class DB
+{
+    protected $conn = null;
+    private $_host = HOST;
+    private $_dbname = DBNAME;
+    private $_user = USER;
+    private $_password = PASSWORD;
+    private $_error;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=" . $this -> _host . ";dbname=" . $this -> _dbname . ";charset=utf8";
+        try {
+            $this -> conn = new PDO($dsn, $this -> _user, $this -> _password);
+        } catch (PDOException $e)
+        {
+            $this -> conn = null;
+            $this -> _error = $e -> getMessage();
+        }
+    }
+
+    public function getError()
+    {
+        return $this -> _error;
+
+    }
+}

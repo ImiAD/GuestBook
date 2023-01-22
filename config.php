8ba5ae67002050af7bd0ae3939bddbc9422ff9e6
Pavel Laukhin <pawel.lauhin@yandex.ru>
2023-01-22 22:52:39 +0300

    config + database

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+CONST HOST = 'localhost';
+CONST USER = 'root';
+CONST PASSWORD = '';
+CONST DBNAME = 'book';
+CONST CHARSET = 'utf8';
+CONST SALT = 'imi';
---
+++ database.sql
@@ -0,0 +1,20 @@
+create table `users` (
+    `id` int(11) not null auto_increment,
+    `username` varchar(100) not null,
+    `email` varchar(150) not null,
+    `password` varchar(100) not null,
+    `first_name` varchar(80) not null,
+    `last_name` varchar(150) not null,
+    `created_at` timestamp default CURRENT_TIMESTAMP,
+    primary key (`id`)
+) ENGINE=innodb default charset=utf8;
+
+create table `comments` (
+    `id` int(11) not null auto_increment,
+    `user_id` int(11) not null,
+    `text` text not null,
+    `created_at` timestamp default CURRENT_TIMESTAMP,
+    primary key (`id`),
+    foreign key (`user_id`) references `users`(`id`)
+        on delete cascade
+) ENGINE=innodb default charset=utf8;
